41f2bd44f38e38b06d35911c75012b5d2071821e
HooDeLin <hoodelin0619@gmail.com>
2018-04-01 14:48:44 +0800

    Initial commit

---
+++ search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import sys
+import getopt
+
+def usage():
+    print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q query-file -o output-file-of-results"
+
+dictionary_file = postings_file = file_of_queries = output_file_of_results = None
+	
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o == '-d':
+        dictionary_file  = a
+    elif o == '-p':
+        postings_file = a
+    elif o == '-q':
+        file_of_queries = a
+    elif o == '-o':
+        file_of_output = a
+    else:
+        assert False, "unhandled option"
+
+if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
+    usage()
+    sys.exit(2)
