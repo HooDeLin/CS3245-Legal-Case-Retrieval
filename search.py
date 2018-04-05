51273f47a47d0a64a83bea416023dfb39c1d6dc4
HooDeLin <hoodelin0619@gmail.com>
2018-04-06 00:00:58 +0800

    Basic wordnet query expansion

--- search.py
+++ search.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 import sys
 import getopt
+import itertools
+from nltk.corpus import wordnet as wn
+
+def parse_and_expand_query(query):
+    queries = [q.strip().lstrip('"').rstrip('"') for q in query.split("AND")]
+    expanded_query = []
+    for query in queries:
+        query_tokens = query.split(" ")
+        syns = []
+        for q in query_tokens:
+            lemmas = list(map(lambda syn: syn.lemmas(), wn.synsets(q)[:2]))
+            lemmas = list(itertools.chain(*lemmas))
+            syns += [str(lemma.name()) for lemma in lemmas]
+        syns = list(map(lambda syn: syn.replace("_", " "), syns))
+        expanded_query.append((query, list(set(syns))))
+    return expanded_query
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q query-file -o output-file-of-results")
@@ -28,3 +44,8 @@ for o, a in opts:
 if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
     usage()
     sys.exit(2)
+
+query_fp = open(file_of_queries, "r")
+
+for line in query_fp:
+    print(parse_and_expand_query(line))
