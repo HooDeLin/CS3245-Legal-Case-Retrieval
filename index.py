0e280056a34cea0c8457b05bb4067bdcfb0c0d59
ChuaPingChan <chua_pingchan@u.nus.edu>
2018-04-08 17:32:01 +0800

    Implement indexing. In-progress.

--- index.py
+++ index.py
@@ -2,7 +2,11 @@
 import re
 import os
 import sys
+import math
 import getopt
+import pickle
+import shelve
+from collections import Counter
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import word_tokenize
@@ -68,6 +72,15 @@ def preprocess_string(raw_string):
     tokens = stem(tokens)
     return tokens
 
+def log_tf(tf):
+    if (tf == 0):
+        return 0
+    else:
+        return 1 + math.log10(tf)
+
+def idf(df, N):
+    return math.log10(N/df)
+
 def main():
     # Command line inputs
     input_directory = output_file_dictionary = output_file_postings = None
@@ -92,9 +105,95 @@ def main():
         usage()
         sys.exit(2)
 
-    df = pd.read_csv(input_directory, index_col=0)
+    print("Reading and processing 'dataset.csv'...")   # TODO: Remove before submission.
+    df = pd.read_csv(input_directory)
+    df = df.set_index("document_id", drop=False)
+    df = df.drop_duplicates(("document_id", "content"), keep='last')    # TODO: Pick highest court. Currently picking the last one.
+    df.sort_index()     # In case doc IDs are not sorted in increasing values
+
+    # Nomenclature of variables: name_dtype
+
+    # TODO: Create 2gram and 3 gram postings
+    # Postings are [docID, normalized tf-idf] pairs
+    postings_1gram_dict = dict()
+    all_docIDs_list = []
+    term_to_idf_dict = dict()
+
+    # Constants
+    IDX_POSTINGS_DOCID = 0
+    IDX_POSTINGS_TFIDF = 1
+
+    # TODO: Extract neutral citations
+
+    # TODO: Refactor this section into a func
+    # First parse of collection -- accum docIDs for each term to compute idf
+    term_to_docIDs_dict = dict()    # Temporary DS
+    for docID in df.index:
+        all_docIDs_list.append(docID)
+
+        raw_content = df.loc[docID, 'content']
+        
+        unique_terms = set(preprocess_string(raw_content))
+        for term in unique_terms:
+            if term not in term_to_docIDs_dict:
+                term_to_docIDs_dict[term] = [docID]
+            else:
+                term_to_docIDs_dict[term].append(docID)
+
+    num_docs = len(all_docIDs_list)
+    for term in term_to_docIDs_dict:
+        term_to_idf_dict[term] = idf(len(term_to_docIDs_dict[term]), num_docs)
+    del term_to_docIDs_dict
+
+    print("Building postings...")   # TODO: Remove before submission.
+    # Second parse of collection to build postings
+    for docID in df.index:  # Note that df.index is already sorted
+        raw_content = df.loc[docID, 'content']
+        
+        terms_list = preprocess_string(raw_content)
+        term_to_tf_dict = dict(Counter(terms_list))
+        term_to_w_td_dict = dict()
+
+        # Compute normalizing factor (magnitude of doc vector)
+        accum_mag = 0   # Cumulative sum of squares of element doc_vec magnitude as normalizing factor
+        for (term, tf) in term_to_tf_dict.items():
+            w_td = log_tf(tf) + term_to_idf_dict[term]
+            accum_mag += w_td ** 2
+        mag_doc_vec = math.sqrt(accum_mag)
+
+        for (term, w_td) in term_to_w_td_dict.items():
+            normalized_w_td = w_td / mag_doc_vec
+            if (term not in postings_1gram_dict):
+                postings_1gram_dict[term] = list()
+            postings_1gram_dict[term].append([docID, normalized_w_td])
+
+    print("Saving 'dictionary.txt' and 'postings.txt'...")  # TODO: Remove before submission.
+    # Save to 'dictionary.txt' and 'postings.txt'
+    # Dictionary maps terms to (offset, idf) tuples
+    # Postings are (docID, normalized w_td) tuples
+    dictionary_shelf = shelve.open(output_file_dictionary)
+
+    # TODO: Naive logging. Remove before submission.
+    log_dictionary_fout = open('log-dictionary.txt', 'w')
+    log_postings_fout = open('log-postings.txt', 'w')
+
+    with open(output_file_postings, 'wb') as postings_file:
+        for term in sorted(postings_1gram_dict):
+            offset = postings_file.tell()
+            dictionary_shelf[term] = (offset, term_to_idf_dict[term])
+            pickle.dump(postings_1gram_dict[term], postings_file)
+
+            # TODO: Naive logging. Remove before submission.
+            log_dictionary_fout.write("'{}' --> {}, {}".format(term, offset, term_to_idf_dict[term]))
+            log_postings_fout.write("'{}' --> {}".format(term, repr(postings_1gram_dict[term])))
+
+    dictionary_shelf.close()
+
+    # TODO: Naive logging. Remove before submission.
+    log_dictionary_fout.close()
+    log_postings_fout.close()
     
-    
+    # TODO: Create retrieval methods for De Lin
 
 ##################################
 # Procedural Program Starts Here #
